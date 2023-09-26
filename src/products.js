cbe54f7e048521cb229fa81c4729597fd6f7d2db
Vinayagabharathi <143339762+Vinayagabharathi@users.noreply.github.com>
2023-09-26 21:40:57 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/products/1.jpg";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.webp";
+import product5 from "./assets/products/5.jpg";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.jpg";
+import product9 from "./assets/products/9.png";
+export const PRODUCTS = [
+    {
+      id: 1,
+      productName: "IPhone",
+      price: 90000,
+      productImage: product1,
+    },
+    {
+      id: 2,
+      productName: "Kotion Each Headphone",
+      price: 3000,
+      productImage: product2,
+    },
+    {
+      id: 3,
+      productName: "Cannon M50 Camera",
+      price: 48000,
+      productImage: product3,
+    },
+    {
+      id: 4,
+      productName: "Asus Vivobook 15",
+      price: 55000,
+      productImage: product4,
+    },
+    {
+      id: 5,
+      productName: "Samsung LED Tv",
+      price: 38000,
+      productImage: product5,
+    },
+    {
+      id: 6,
+      productName: "PlayStation 5",
+      price: 70000,
+      productImage: product6,
+    },
+    {
+      id: 7,
+      productName: "EPSON Projecter",
+      price: 20000,
+      productImage: product7,
+    },
+    {
+      id: 8,
+      productName: "Creative Bluetooth Speaker",
+      price: 1500,
+      productImage: product8,
+    },
+    {
+      id: 9,
+      productName: "EPSON Printer",
+      price: 35000,
+      productImage: product9,
+      
+    },
+];
